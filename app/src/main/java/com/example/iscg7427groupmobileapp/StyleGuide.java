38286b4fcd2e99a3238a7fe0dd752e10eb231f79
hzzyn <zyniiee@gmail.com>
2024-05-17 15:09:21 +1200

    Update styleguide

---
+++ app/src/main/java/com/example/iscg7427groupmobileapp/StyleGuide.java
@@ -0,0 +1,31 @@
+package com.example.iscg7427groupmobileapp;
+
+import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.style.UnderlineSpan;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class StyleGuide extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_style_guide);
+        TextView underlineTextView = findViewById(R.id.underlineText);
+        updateUnderlinedTextView(underlineTextView, "This is an underline text");
+    }
+
+    // Method to update TextView with underlined text
+    private void updateUnderlinedTextView(TextView textView, String targetText) {
+        String text = targetText;
+        SpannableString spannableString = new SpannableString(text);
+
+        // Apply UnderlineSpan to the entire text
+        spannableString.setSpan(new UnderlineSpan(), 0, text.length(), SpannableString.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+        // Set the modified SpannableString to the TextView
+        textView.setText(spannableString);
+    }
+}
